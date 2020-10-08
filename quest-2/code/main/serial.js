79ac4fae3c2edbc7b1af041cff4985760ab63949
Allen Zou <allenz1120@gmail.com>
2020-10-07 20:54:22 -0400

    added canvasJS but not working

--- quest-2/code/main/serial.js
+++ quest-2/code/main/serial.js
@@ -1,7 +1,9 @@
 const SerialPort = require("serialport");
-var http = require("http");
 var fs = require("fs");
 const path = require("path");
+var app = require('express')();
+var http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 const port = new SerialPort("COM5", {
   baudRate: 115200,
@@ -11,6 +13,7 @@ let data;
 let thermistorData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let irData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let ultrasonicData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 port.on("readable", function () {
   let data = String(port.read()).split(',');
   // console.log(data.split(','));
@@ -27,8 +30,78 @@ port.on("readable", function () {
     ultrasonicData.push(Number(data[1].slice(0, -2)));
     // console.log('u', data[1].slice(0, -2))
   }
-  console.log('THERMISTOR', thermistorData);
-  console.log('IR', irData);
-  console.log('ULTRASONIC', ultrasonicData);
+  // console.log('THERMISTOR', thermistorData);
+  // console.log('IR', irData);
+  // console.log('ULTRASONIC', ultrasonicData);
 
+});
+
+
+var chartSettings = {
+  animationEnabled: true,
+  title: {
+    text: "Thermistor : Ultrasonic : IR Rangefinder"
+  },
+  axisX: {
+    interval: 1,
+    intervalType: "Sensor Data"
+  },
+  axisY: {
+    title: "Temperature / Distance",
+    prefix: ""
+  },
+  legend: {
+    cursor: "pointer",
+    fontSize: 16,
+    itemclick: toggleDataSeries
+  },
+  toolTip: {
+    shared: true
+  },
+  data: [{
+    name: "Thermistor",
+    type: "spline",
+    yValueFormatString: "#C",
+    showInLegend: true,
+    dataPoints: thermistorData
+  },
+  {
+    name: "Infared Sensor",
+    type: "spline",
+    yValueFormatString: "#cm",
+    showInLegend: true,
+    dataPoints: irData
+  },
+  {
+    name: "Ultrasonic Senor",
+    type: "spline",
+    yValueFormatString: "#cm",
+    showInLegend: true,
+    dataPoints: ultrasonicData
+  }]
+};
+
+function toggleDataSeries(e) {
+  if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+    e.dataSeries.visible = false;
+  }
+  else {
+    e.dataSeries.visible = true;
+  }
+  chart.render();
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/canvasJS.html');
+});
+
+io.on("connect", (socket) => {
+  console.log("CONNECTED");
+  let counter = 0;
+  io.emit("", chartSettings);
+
+});
+
+http.listen(8080, () => {
+  console.log("go to http://localhost:8080");
 });
